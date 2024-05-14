a174139ca24f069d585a1c9ec9dd1ce4544c36fd
anzisheng <anzisheng@163.com>
2024-05-13 19:22:14 -0700

    yolov3 rknn is ok for 514

--- configs/_base_/backends/rknn.py
+++ configs/_base_/backends/rknn.py
@@ -1,10 +1,10 @@
 backend_config = dict(
     type='rknn',
     common_config=dict(
-        target_platform='rv1126',  # 'rk3588'
+        target_platform='rk3588',  # 'rv1126'
         optimization_level=1),
     quantization_config=dict(
         do_quantization=True,
         dataset=None,
         pre_compile=False,
-        rknn_batch_size=-1))
+        rknn_batch_size=1)) #anzisheng -1
